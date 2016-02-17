e5186f606efe0999915787be9ca2c9c38a83c2c8
Thomas Brown <tbrown2@luc.edu>
2016-02-16 21:39:21 -0600

    class additions

--- src/com/fms/model/facility/Building.java
+++ src/com/fms/model/facility/Building.java
@@ -2,4 +2,40 @@ package com.fms.model.facility;
 
 public class Building {
 	
+	private String BuildingID;
+	private String AddressID;
+	private int Capacity;
+	private int roomsFilled;
+	private int IssueCount;
+	
+	public String getBuildingID() {
+		return BuildingID;
+	}
+	public void setBuildingID(String buildingID) {
+		BuildingID = buildingID;
+	}
+	public String getAddressID() {
+		return AddressID;
+	}
+	public void setAddressID(String addressID) {
+		AddressID = addressID;
+	}
+	public int getCapacity() {
+		return Capacity;
+	}
+	public void setCapacity(int capacity) {
+		Capacity = capacity;
+	}
+	public int getRoomsFilled() {
+		return roomsFilled;
+	}
+	public void setRoomsFilled(int roomsFilled) {
+		this.roomsFilled = roomsFilled;
+	}
+	public int getIssueCount() {
+		return IssueCount;
+	}
+	public void setIssueCount(int issueCount) {
+		IssueCount = issueCount;
+	}
 }
--- src/com/fms/model/facility/Room.java
+++ src/com/fms/model/facility/Room.java
@@ -1,5 +1,54 @@
 package com.fms.model.facility;
 
 public class Room {
-
+	private String RoomID;
+	private int Floor;
+	private int RoomNo;
+	private String BuildingID;
+	private String PrimaryTenantID;
+	private boolean vacant;
+	private int TenantNumber;
+	
+	public String getRoomID() {
+		return RoomID;
+	}
+	public void setRoomID(String roomID) {
+		RoomID = roomID;
+	}
+	public int getFloor() {
+		return Floor;
+	}
+	public void setFloor(int floor) {
+		Floor = floor;
+	}
+	public int getRoomNo() {
+		return RoomNo;
+	}
+	public void setRoomNo(int roomNo) {
+		RoomNo = roomNo;
+	}
+	public String getBuildingID() {
+		return BuildingID;
+	}
+	public void setBuildingID(String buildingID) {
+		BuildingID = buildingID;
+	}
+	public String getPrimaryTenantID() {
+		return PrimaryTenantID;
+	}
+	public void setPrimaryTenantID(String primaryTenantID) {
+		PrimaryTenantID = primaryTenantID;
+	}
+	public boolean isVacant() {
+		return vacant;
+	}
+	public void setVacant(boolean vacant) {
+		this.vacant = vacant;
+	}
+	public int getTenantNumber() {
+		return TenantNumber;
+	}
+	public void setTenantNumber(int tenantNumber) {
+		TenantNumber = tenantNumber;
+	}
 }
