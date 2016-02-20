6041960083f37f14415b3bd892e697613e60a8ae
Thomas Brown <tbrown2@luc.edu>
2016-02-20 17:53:36 -0600

    finished room class

--- src/com/fms/model/facility/Room.java
+++ src/com/fms/model/facility/Room.java
@@ -5,11 +5,11 @@ import com.fms.model.users.*;
 
 public class Room {
 	private String RoomID;
-	private int Floor;
 	private int RoomNo;
-	private Tenants PrimaryTenantID;
 	private boolean vacant;
 	private ArrayList<Tenants> tenants = new ArrayList<Tenants>();
+	
+	public Room(){}
 	
 	public String getRoomID() {
 		return RoomID;
@@ -17,29 +17,11 @@ public class Room {
 	public void setRoomID(String roomID) {
 		RoomID = roomID;
 	}
-	public int getFloor() {
-		return Floor;
-	}
-	public void setFloor(int floor) {
-		Floor = floor;
-	}
 	public int getRoomNo() {
 		return RoomNo;
 	}
 	public void setRoomNo(int roomNo) {
 		RoomNo = roomNo;
-	}
-	public String getBuildingID() {
-		return BuildingID;
-	}
-	public void setBuildingID(String buildingID) {
-		BuildingID = buildingID;
-	}
-	public Tenants getPrimaryTenant() {
-		return PrimaryTenantID;
-	}
-	public void setPrimaryTenantID(Tenants primaryTenantID) {
-		PrimaryTenantID = primaryTenantID;
 	}
 	public boolean isVacant() {
 		return vacant;
@@ -55,5 +37,13 @@ public class Room {
 	}
 	public ArrayList<Tenants> getTenants(){
 		return tenants;
+	}
+	public Tenants getPrimaryTenant(){
+		for (Tenants tenant: tenants){
+			if (tenant.isPrimary()){
+				return tenant;
+			}
+		}
+		return tenants.get(0);
 	}
 }
