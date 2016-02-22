d009645fee55a116afa44d29ae956b1fbd19d83a
Tyler Pearson <tpearson1994@gmail.com>
2016-02-22 16:49:48 -0600

    added issues and static information

--- src/com/fms/view/FacilityClient1.java
+++ src/com/fms/view/FacilityClient1.java
@@ -12,12 +12,22 @@ import com.fms.model.users.*;
 public class FacilityClient1 {
 	public static void main (String args[]) throws Exception {
 
-		// TODO create issue types
-		// TODO create employees - Set ID
-		// TODO Add specialties to employee
-		// TODO create issue
+		// Instantiating issue types
+		IssueType plumb = new IssueType("Plumbing", "1");
+		IssueType elec = new IssueType("Electrical", "2");
+		IssueType carp = new IssueType("Carpentry", "3");
 
-	//instantiating objects and using methods in Facility 
+		// Instantiating employees
+		Employees one = new Employees("John", "Smith", "1");
+		Employees two = new Employees("George", "Townsend", "2");
+
+		// Adding specialties to employees
+		one.addSpecialty(plumb);
+		two.addSpecialty(elec);
+		two.addSpecialty(carp);
+
+
+	//instantiating objects and using methods in Facility
 	Address testAddress = new Address ();
 	testAddress.setAddressID("1");
 	testAddress.setAddressNumber(6459);
@@ -25,9 +35,9 @@ public class FacilityClient1 {
 	testAddress.setCity("Chicago");
 	testAddress.setState("Illinois");
 	testAddress.setZip(49120);
-	
+
 	Building testBuilding = new Building("1", 2, 2, testAddress);
-	
+
 	//instantiating objects and using methods in Users
 	Tenants t1 = new Tenants();
 	Tenants t2 = new Tenants();
@@ -38,10 +48,24 @@ public class FacilityClient1 {
 	t2.setLastName("Pearson");
 	t2.setTenID("2");
 	t2.setPrimary(true);
-	
+
 	testBuilding.addTenantToRoom(t1, 201);
 	testBuilding.addTenantToRoom(t2, 201);
 	System.out.println(testBuilding.toString());
+		System.out.println();
+
+		// Create issue
+		Issues ione = new Issues();
+		ione.setIssueid("1");
+		ione.setIssueType(plumb);
+		ione.setAssignee(one);
+		ione.setRoomLocation(testBuilding.getRoom(3));
+		ione.setComments("The sink in 201 has a leaky faucet.");
+		ione.setBuildingLocation(testBuilding);
+		System.out.println(ione.toString());
+
+
+
 	//instantiating objects and using methods in Maintenance
 	}
 }
