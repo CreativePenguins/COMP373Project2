1f08fbc751add74afc6d028315d40bdb64d2f060
Thomas Brown <tbrown2@luc.edu>
2016-04-03 15:23:11 -0500

    new changes

--- src/com/fms/view/FacilityClient1.java
+++ src/com/fms/view/FacilityClient1.java
@@ -4,11 +4,25 @@ import com.fms.model.facility.*;
 import com.fms.model.maintenance.*;
 import com.fms.model.users.*;
 import com.fms.model.service.*;
+import com.fms.view.*;
 /**
  * This class will employ the object classes and their methods
  * @author Tom and Tyler
  *
  */
 public class FacilityClient1 {
-	public static void main (String args[]) throws Exception {}
+	public static void main (String args[]) throws Exception {
+
+		FacilityClientManager fcm = new FacilityClientManager();
+
+		//add a building into our database, works with user input to enter a new building into our database
+		fcm.addNewFacility();
+
+		//print each facility and their details
+		fcm.listFacilites();
+
+		//removes a building and their details from our database,
+		// works with user input to determine which building we are trying to remove
+		fcm.removeFacility();
+	}
 }
--- src/com/fms/view/FacilityClientManager.java
+++ src/com/fms/view/FacilityClientManager.java
@@ -1,13 +1,29 @@
 package com.fms.view;
+import com.fms.model.facility.*;
+import java.util.ArrayList;
 
 public class FacilityClientManager {
 	FacilityClientManager(){};
 	
 	//TODO define what each of these methods are accomplishing and code them
 	//this class will talk to the service class
-	public void listFacilites(){};
-	public void getFacilityInformation(){};
-	public void requestAvailableCapacity(){};
+	public void listFacilites(){
+		//pulls all the buildings from our database and assigns them to list
+		//for each building we need to
+		//list how many rooms each building has, and their address by invoking get facilityinformation is evoked inside this method
+	}
+	public void getFacilityInformation(Building b){
+		// this method will print how many rooms each building has, the buildings address
+		//invokes request available capacity method for each room
+	};
+	public int requestAvailableCapacity(ArrayList<Room> r){
+		//pass in a list of rooms and return an integer that represents how many empty rooms in the list
+		int count = 0;
+		for (Room room: r){
+			if (room.isVacant()) count++;
+		}
+		return count;
+	};
 	public void addNewFacility(){};
 	public void addFacilityDetail(){};
 	public void removeFacility(){};
