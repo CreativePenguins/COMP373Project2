f73008d2e0e2c1742ca96619cf5b604de21878aa
Tyler Pearson <tpearson1994@gmail.com>
2016-04-04 21:57:40 -0500

    spring

--- src/com/fms/model/users/EmployeesImpl.java
+++ src/com/fms/model/users/EmployeesImpl.java
@@ -36,18 +36,22 @@ public class EmployeesImpl extends UsersImpl implements Employees  {
     public IssueType getSpecialty1(){
         return specialty1;
     }
+
+    @Autowired
     public void setSpecialty1(IssueType t){
         specialty1 = t;
     }
     public IssueType getSpecialty2(){
         return specialty2;
     }
+    @Autowired
     public void setSpecialty2(IssueType t){
         specialty2 = t;
     }
     public IssueType getSpecialty3(){
         return specialty3;
     }
+    @Autowired
     public void setSpecialty3(IssueType t){
         specialty3 = t;
     }
