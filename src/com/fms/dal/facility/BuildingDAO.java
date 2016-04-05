81533992f88be20485bc03e3107f8b9203bae2f3
Thomas Brown <tbrown2@luc.edu>
2016-04-04 22:19:34 -0500

    something something something

--- src/com/fms/dal/facility/BuildingDAO.java
+++ src/com/fms/dal/facility/BuildingDAO.java
@@ -1,41 +1,16 @@
 package com.fms.dal.facility;
 
-import com.fms.model.facility.Building;
-import org.hibernate.Query;
-import org.hibernate.Session;
 import com.fms.dal.HibernatePGSQLHelper;
+import com.fms.model.facility.Building;
 import com.fms.model.facility.BuildingImpl;
-
-import java.util.List;
+import org.hibernate.Session;
 
 public class BuildingDAO {
 	public BuildingDAO(){};
 	
 	public BuildingImpl getBuilding(int buildingID) {
-		System.out.println("*************** Searcing for customer address information with ID ...  " + customerId);
-		Session session = HibernatePGSQLHelper.getSessionFactory().getCurrentSession();
-		session.beginTransaction();
-
-		//Address billingAdd = (AddressImpl) session.load(AddressImpl.class, customerId);
-
-		Query getAddresstQuery = session.createQuery("From AddressImpl where customerId=:customerId");
-		getAddresstQuery.setString("customerId", customerId);
-
-		System.out.println("*************** Retrieve Query is ....>>\n" + getAddresstQuery.toString());
-
-		List addresses = getAddresstQuery.list();
-		System.out.println("Getting Book Details using HQL. \n" + addresses.get(0));
-
-		System.out.println("*************** Retrieve Query is ....>>\n" + addresses.get(0).toString());
-
-		session.getTransaction().commit();
-		return (Address)addresses.get(0);
-
-	} catch (Exception e) {
-		e.printStackTrace();
-	}
-	return null;
-}
+		BuildingImpl b = new BuildingImpl();
+		return b;
 	}
 
 	public /**ArrayList<Building>**/ int getBuildings(){
